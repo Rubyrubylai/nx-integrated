7c08eeb34d5ce924e13b32375ec7277018c44343
rubylai <rubylai@rubylais-MacBook-Pro.local>
2023-05-15 18:36:49 +0800

    Add schema lib

--- apps/server/src/payments/payments.service.ts
+++ apps/server/src/payments/payments.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { GetPaymentsArgs } from './dto/payments.dto';
+import { schema } from '@nx-integrated/schema';
 
 @Injectable()
 export class PaymentsService {
   async findOne(args: GetPaymentsArgs) {
+    console.log(schema)
     return {
       id: args.id,
       description: 'ApplePay',
