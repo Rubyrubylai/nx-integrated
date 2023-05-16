3d793e69123fa9c2ff1f6da1d2f32d80014833de
ruby <ruby@rubydeMacBook-Air.local>
2023-05-17 01:15:24 +0800

    test

--- apps/server/src/payments/payments.resolver.ts
+++ apps/server/src/payments/payments.resolver.ts
@@ -12,6 +12,7 @@ export class PaymentsResolver {
 
 	@Query(returns => Payments, { name: 'payments'})
 	async getPayment(@Args() args: GetPaymentsArgs): Promise<Payments> {  // 自定義 dto
+		console.log('test')
 		return await this.paymentsService.findOne(args)
 	}
 }
