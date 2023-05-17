914da69199c0c24554575f227f34b629b39b9e11
rubylai <rubylai@rubylais-MacBook-Pro.local>
2023-05-17 16:52:16 +0800

    test

--- apps/server/src/payments/payments.resolver.ts
+++ apps/server/src/payments/payments.resolver.ts
@@ -11,7 +11,6 @@ export class PaymentsResolver {
   @Query((returns) => Payments, { name: 'payments' })
   async getPayment(@Args() args: GetPaymentsArgs): Promise<Payments> {
     // 自定義 dto
-    console.log('test');
     return await this.paymentsService.findOne(args);
   }
 }
