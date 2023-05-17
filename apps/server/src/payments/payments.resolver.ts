b3426560ecdcbcfae9e628a6796c41f8aba5652f
rubylai <rubylai@rubylais-MacBook-Pro.local>
2023-05-17 15:17:11 +0800

    test build image

--- apps/server/src/payments/payments.resolver.ts
+++ apps/server/src/payments/payments.resolver.ts
@@ -6,13 +6,12 @@ import { PaymentsService } from './payments.service';
 
 @Resolver(Payments)
 export class PaymentsResolver {
-	constructor(
-		private paymentsService: PaymentsService
-	) {}
+  constructor(private paymentsService: PaymentsService) {}
 
-	@Query(returns => Payments, { name: 'payments' })
-	async getPayment(@Args() args: GetPaymentsArgs): Promise<Payments> {  // 自定義 dto
-		console.log('test')
-		return await this.paymentsService.findOne(args)
-	}
+  @Query((returns) => Payments, { name: 'payments' })
+  async getPayment(@Args() args: GetPaymentsArgs): Promise<Payments> {
+    // 自定義 dto
+    console.log('test');
+    return await this.paymentsService.findOne(args);
+  }
 }
